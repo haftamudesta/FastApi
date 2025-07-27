5c4f1374e5d9d4bee7da0366d01fa2cb7a1131a8
haftamudesta <desta451616@hotmail.com>
2025-07-27 07:21:24 -0800

    Add some  front end set up

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Game</h1>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/LoadingStatus.jsx
@@ -0,0 +1,15 @@
+
+
+const LoadingStatus = ({theme}) => {
+  return (
+    <div>
+        <h1>generating Your {theme} Story</h1>
+        <div>
+                <div>spinner</div>
+        </div>
+        <p>Please wait while generating your story...</p>
+    </div>
+  )
+}
+
+export default LoadingStatus
